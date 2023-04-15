5e83184e9fe45534c6d77ea180ba4efb13974672
xcxug <1286450009@qq.com>
2023-04-16 01:42:31 +0800

    day14

--- src/pages/home/goods/details_content.js
+++ src/pages/home/goods/details_content.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import config from '../../../assets/js/conf/config.js';
+import { request } from "../../../assets/js/libs/request";
+import { localParam } from '../../../assets/js/utils/util.js';
 import Css from '../../../assets/css/home/goods/details_content.module.css';
 
 class DetailsContent extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            bodys: '',
+            gid: props.location.search !== '' ? localParam(props.location.search).search.gid : ''
+        }
+    }
+
+    componentDidMount() {
+        let sUrl = config.baseUrl + "/api/home/goods/info?gid=" + this.state.gid + "&type=details&token=" + config.token;
+        request(sUrl).then(res => {
+            if (res.code === 200) {
+                this.setState({ bodys: res.data.bodys });
+            }
+        });
+    }
+
     componentWillUnmount() {
         this.setState = (_state, _callback) => {
             return;
@@ -11,7 +32,7 @@ class DetailsContent extends Component {
     render() {
         return (
             <div className={Css['page']}>
-                商品详情
+                <div className={Css['content']} dangerouslySetInnerHTML={{ __html: this.state.bodys }}></div>
             </div>
         );
     }
