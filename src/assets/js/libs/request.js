5d84b329904a3ec44b05e441da25de574b0bcf0d
xcxug <1286450009@qq.com>
2023-05-04 23:17:32 +0800

    day18

--- src/assets/js/libs/request.js
+++ src/assets/js/libs/request.js
@@ -1,9 +1,30 @@
 import ReactDOM from 'react-dom';
 let oLoad = ReactDOM.findDOMNode(document.getElementById("page-load"));
 
-function request(pUrl, pType = 'GET') {
+function request(pUrl, pType = 'get'.toLocaleLowerCase(), data = {}) {
     showLoad();
-    return fetch(pUrl, { method: pType }).then(res => {
+    let config = {}, headers = {}, params = '';
+    if (pType === "get".toLocaleLowerCase()) {
+        config = {
+            method: pType
+        }
+    } else {
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+        if (data instanceof Object) {
+            for (let key in data) {
+                params += `&${key}=${encodeURIComponent(data[key])}`;
+            }
+            params = params.slice(1);
+        }
+        config = {
+            method: pType,
+            headers,
+            body: params
+        }
+    }
+    return fetch(pUrl, config).then(res => {
         hideLoad();
         return res.json();
     });
