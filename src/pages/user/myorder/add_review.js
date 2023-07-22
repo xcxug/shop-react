0757519d9f1bf38996ab7766572f05dbf0ffb383
xxc <1286450009@qq.com>
2023-07-22 16:17:09 +0800

    day35

--- src/pages/user/myorder/add_review.js
+++ src/pages/user/myorder/add_review.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { connect } from "react-redux";
-import { safeAuth } from '../../../assets/js/utils/util.js';
+import config from '../../../assets/js/conf/config.js';
+import { localParam, safeAuth } from '../../../assets/js/utils/util.js';
+import { request } from '../../../assets/js/libs/request.js';
+import { Toast } from 'antd-mobile';
 import SubHeaderComponent from '../../../components/header/subheader';
 import Css from '../../../assets/css/user/myorder/add_review.module.css';
 
@@ -9,6 +12,18 @@ class AddReview extends Component {
         super(props);
 
         safeAuth(props);
+
+        this.state = {
+            services: [],
+            content: ''
+        };
+        this.gid = props.location.search ? localParam(props.location.search).search.gid : '';
+        this.ordernum = props.location.search ? localParam(props.location.search).search.ordernum : '';
+        this.isSubmit = true;
+    }
+
+    componentDidMount() {
+        this.getServices();
     }
 
     componentWillUnmount() {
@@ -17,35 +32,146 @@ class AddReview extends Component {
         }
     }
 
+    // 选择评价分数
+    selectScore(index, index2) {
+        let services = this.state.services;
+        for (let i = 0; i < services[index].scores.length; i++) {
+            services[index].scores[i].checked = false;
+        }
+        for (let i = 0; i <= index2; i++) {
+            services[index].scores[i].checked = true;
+        }
+        this.setState({ services: services });
+    }
+
+    // 获取星级选项数据
+    getServices() {
+        let url = config.baseUrl + "/api/home/reviews/service?token=" + config.token;
+        request(url).then(res => {
+            if (res.code === 200) {
+                let services = res.data;
+                for (let i = 0; i < services.length; i++) {
+                    services[i].scores = [
+                        { checked: false, score: 1 },
+                        { checked: false, score: 2 },
+                        { checked: false, score: 3 },
+                        { checked: false, score: 4 },
+                        { checked: false, score: 5 }
+                    ]
+                }
+                this.setState({ services: services })
+            }
+        })
+    }
+
+    // 提交评价
+    submitSave() {
+        if (this.isSubmit) {
+            this.isSubmit = false;
+            let isChecked = false, rsdata = [], scores = [], score = 0;
+            for (let i = 0; i < this.state.services.length; i++) {
+                isChecked = false;
+                for (let j = 0; j < this.state.services[i].scores.length; j++) {
+                    if (this.state.services[i].scores[j].checked) {
+                        isChecked = true;
+                        break;
+                    }
+                }
+                if (!isChecked) {
+                    this.isSubmit = true;
+                    Toast.show({
+                        content: '请选择' + this.state.services[i].title,
+                        afterClose: () => {
+                            console.log('after');
+                        },
+                    })
+                    return;
+                }
+            }
+            if (this.state.content.match(/^\s*$/)) {
+                this.isSubmit = true;
+                Toast.show({
+                    content: '请输入评价内容',
+                    afterClose: () => {
+                        console.log('after');
+                    },
+                })
+                return;
+            }
+            // 组装评价数据
+            for (let i = 0; i < this.state.services.length; i++) {
+                scores = [];
+                for (let j = 0; j < this.state.services[i].scores.length; j++) {
+                    if (this.state.services[i].scores[j].checked) {
+                        scores.push(this.state.services[i].scores[j].score)
+                    }
+                }
+                score = scores[scores.length - 1];
+                rsdata.push({ gid: this.gid, myid: this.props.state.user.uid, rsid: this.state.services[i].rsid, score: score });
+            }
+
+            let data = {
+                uid: this.props.state.user.uid,
+                gid: this.gid,
+                ordernum: this.ordernum,
+                content: this.state.content,
+                rsdata: JSON.stringify(rsdata)
+            }
+            let url = config.baseUrl + "/api/home/reviews/add?token=" + config.token;
+            request(url, 'post', data).then(res => {
+                if (res.code === 200) {
+                    Toast.show({
+                        content: res.data,
+                        afterClose: () => {
+                            console.log('after');
+                            this.props.history.goBack();
+                        },
+                    })
+                } else {
+                    this.isSubmit = true;
+                    Toast.show({
+                        content: res.data,
+                        afterClose: () => {
+                            console.log('after');
+                        },
+                    })
+                }
+            });
+        }
+
+    }
+
     render() {
         return (
             <div className={Css['page']}>
                 <SubHeaderComponent title="评价"></SubHeaderComponent>
                 <div className={Css['main']}>
-                    <ul className={Css['service']}>
-                        <li>整体</li>
-                        <li>
-                            <div className={Css['stars'] + " " + Css['active']}></div>
-                            <div className={Css['stars']}></div>
-                            <div className={Css['stars']}></div>
-                            <div className={Css['stars']}></div>
-                            <div className={Css['stars']}></div>
-                        </li>
-                    </ul>
-                    <ul className={Css['service']}>
-                        <li>服务</li>
-                        <li>
-                            <div className={Css['stars'] + " " + Css['active']}></div>
-                            <div className={Css['stars']}></div>
-                            <div className={Css['stars']}></div>
-                            <div className={Css['stars']}></div>
-                            <div className={Css['stars']}></div>
-                        </li>
-                    </ul>
+                    {
+                        this.state.services.length > 0 ?
+                            this.state.services.map((item, index) => {
+                                return (
+                                    <ul className={Css['service']} key={index}>
+                                        <li>{item.title}</li>
+                                        <li>
+                                            {
+                                                item.scores.length > 0 ?
+                                                    item.scores.map((item2, index2) => {
+                                                        return (
+                                                            <div key={index2} className={item2.checked ? Css['stars'] + " " + Css['active'] : Css['stars']} onClick={this.selectScore.bind(this, index, index2)}></div>
+                                                        )
+                                                    })
+                                                    : ""
+                                            }
+                                        </li>
+                                    </ul>
+                                )
+                            })
+                            : ""
+                    }
                     <div className={Css['content-wrap']}>
-                        <textarea placeholder="来分享你的消费感受吧!"></textarea>
+                        <textarea placeholder="来分享你的消费感受吧!" onChange={(e) => { this.setState({ content: e.target.value }) }}></textarea>
                     </div>
-                    <button className={Css['submit']} type="button">提交</button>
+                    <button className={Css['submit']} type="button" onClick={this.submitSave.bind(this)}>提交</button>
                 </div>
             </div>
         );
